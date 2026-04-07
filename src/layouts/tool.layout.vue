db8e7f648fef1ec759556256ee94e111c3dacc6a
SteveTheKiller <steve@thekiller.net>
2026-04-06 20:52:56 -0700

    tab name capitalization

--- src/layouts/tool.layout.vue
+++ src/layouts/tool.layout.vue
@@ -10,7 +10,7 @@ import type { Tool } from '@/tools/tools.types';
 const route = useRoute();
 
 const head = computed<HeadObject>(() => ({
-  title: `${route.meta.name} - killer tools`,
+  title: `${route.meta.name} - Killer Tools`,
   meta: [
     {
       name: 'description',
